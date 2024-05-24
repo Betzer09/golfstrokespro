076d5cea6b91d0a06d051943770c98734657baaa
Austin Betzer <austin.betzer@Austins-Macbook-Aptive.local>
2024-05-23 19:37:18 -0600

    You can now end a game, and a new game is started

--- GolfTrackPro/Views/Tabs/Game/GolfRoundView.swift
+++ GolfTrackPro/Views/Tabs/Game/GolfRoundView.swift
@@ -9,41 +9,43 @@ import SwiftData
 
 struct GolfRoundView: View {
     @Environment(\.modelContext) var modelContext
-    @Query(sort: \Game.createdAt, order: .forward) private var queryiedGames: [Game] = []
-    @State private var numberOfHoles: Int = 18
+    // TODO: Muliple games
+    // I'm going to have to figure out some sort of flow if there are
+    // more than one games returned.
+    @Query(filter: #Predicate<Game> { game in
+        game.completedAt == nil
+    }, sort: \Game.createdAt, order: .reverse) private var queryiedGames: [Game]
+    private var numberOfHoles: Int = 18
+
     var body: some View {
         NavigationView {
             VStack {
-
-                Picker("Number of Holes", selection: $numberOfHoles) {
-                    Text("9 Holes").tag(9)
-                    Text("18 Holes").tag(18)
-                }
-                .pickerStyle(SegmentedPickerStyle())
-                .padding()
-
                 let scores = queryiedGames.first?.scores ?? []
                 List(scores.sorted(by: { $0.hole < $1.hole }), id: \.self) { score in
-                    NavigationLink(destination: DetailView(hole: score.hole,
-                                                           score: score)) {
-                        HoleScoreView(hole: score.hole,
-                                      score: score)
+                    NavigationLink(destination: DetailView(hole: score.hole, score: score)) {
+                        HoleScoreView(hole: score.hole, score: score)
                     }
                 }
 
                 HStack {
                     Text("Total Score")
                     Spacer()
-                    Text("\(totalScore())") 
+                    Text("\(totalScore())")
                         .bold()
                 }
                 .padding()
             }
+            .navigationTitle("⛳️ GolfTrack Pro")
+            .navigationBarItems(trailing: Button(action: endGame) {
+                Text("End Game")
+                    .foregroundColor(.red)
+            })
             .onAppear(perform: loadScores)
         }
     }
 
     private func loadScores() {
+        print("Number of persisted games: \(queryiedGames.count)")
         if queryiedGames.isEmpty {
             print("Creating a new game of scores.")
             startGame()
@@ -52,12 +54,18 @@ struct GolfRoundView: View {
 
     private func startGame() {
         let game = Game()
-        let scores = (1...$numberOfHoles.wrappedValue).map { Score(game:game, hole: $0) }
+        let scores = (1...numberOfHoles).map { Score(game: game, hole: $0) }
         game.scores = scores
         modelContext.insert(game)
     }
 
-    func totalScore() -> Int {
+    private func endGame() {
+        let game = queryiedGames.first
+        game?.completedAt = Date()
+        startGame()
+    }
+
+    private func totalScore() -> Int {
         if let game = queryiedGames.first {
             return game.scores.reduce(0) { $0 + $1.swings.count }
         } else {
@@ -65,11 +73,6 @@ struct GolfRoundView: View {
         }
     }
 }
-
-#Preview {
-    GolfRoundView()
-}
-
 
 #Preview {
     GolfRoundView()
